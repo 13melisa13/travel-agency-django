cc625e0a02d93a438e2cd8e14b78de43a24f3a5f
13melisa13 <isaeva.a.v@edu.mirea.ru>
2023-11-27 17:33:59 +0300

    init4fix

--- travel_agency/urls.py
+++ travel_agency/urls.py
@@ -31,5 +31,5 @@ urlpatterns = [
     path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('admin/', admin.site.urls),
-    path('countries/', CountryList.as_view(), name='countries-list')
+    # path('countries/', CountryList.as_view(), name='countries-list')
 ]
